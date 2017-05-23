3174a09ee9663a20e00487df6ca79f08ab5f95ac
Fiorella De Luca <deluca_fiorella@yahoo.it>
2017-05-23 19:08:58 +0200

    I have added some models and correct some errors

--- django_freeradius/models.py
+++ django_freeradius/models.py
@@ -1,37 +1,52 @@
 from django.db import models
-
 # Create your models here.
-class Radius_group(models.Model):
-    id = models.UUIDField(primary_key=True, )
-    groupname = models.CharField(max_length=255, unique=True)
-    priority = models.IntegerField(default=1)
-    creation date = models.DateField()
-    modification date = models.DateField()
-    notes = models.CharField()
 
-class Radius_group_users(models.Model):
-    username = models.CharField(max_length=64,unique=True,)
-    id = models.UUIDField(primary_key=True, )
-    groupname = models.CharField(max_length=255, unique=True)
+
+class RadiusGroup(models.Model):
+    id = models.UUIDField(primary_key=True)
+    group_name = models.CharField(max_length=255, unique=True)
+    priority = models.IntegerField(default=1)
+    creation_date = models.DateField(null=True)
+    modification_date = models.DateField(null=True)
+    notes = models.CharField(max_length=64, blank=True)
+
+    def __unicode__(self):
+        return self.group_name
+
+    class Meta:
+        ordering = ['group_name']
+
+
+class RadiusGroupUsers(models.Model):
+    username = models.CharField(max_length=64, unique=True)
+    id = models.UUIDField(primary_key=True)
+    group_name = models.CharField(max_length=255, unique=True)
     radius_replies = models.ManyToManyField('radius_replies', blank=True)
     radius_checks = models.ManyToManyField('Radcheck', blank=True)
 
-class Radius_replies(models.Model):
+
+class RadiusReplies(models.Model):
     username = models.CharField(max_length=64)
+    value = models.CharField(max_length=40)
+    op = models.CharField(max_length=2)
+    attribute = models.CharField(max_length=64)
 
 
-class Radius_checks(model.Models):
+class RadiusChecks(models.Model):
     username = models.CharField(max_length=64)
-
+    value = models.CharField(max_length=40)
+    op = models.CharField(max_length=2)
+    attribute = models.CharField(max_length=64)
 
 
 class Radacct(models.Model):
+    username = models.CharField(max_length=25, null=True)
 
 
 class Nas(models.Model):
-    nasname = models.CharField(max_length=128, unique=True, help_text='NAS Name (or IP address)')
-    shortname = models.CharField(max_length=32)
-    type = models.CharField(max_length=30, choices=NAS_TYPES)
+    NasName = models.CharField(max_length=128, unique=True, help_text='NAS Name (or IP address)')
+    ShortName = models.CharField(max_length=32)
+    type = models.CharField(max_length=30)
     secret = models.CharField(max_length=60, help_text='Shared Secret')
     ports = models.IntegerField(blank=True, null=True)
     community = models.CharField(max_length=50, blank=True, null=True)
