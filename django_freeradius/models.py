d730066fa834f1d22b7d92cd5338049ed1b50d77
Fiorella De Luca <deluca_fiorella@yahoo.it>
2017-05-24 14:44:25 +0200

    [models] Renamed models fields to comply with codestyle

--- django_freeradius/models.py
+++ django_freeradius/models.py
@@ -1,70 +1,108 @@
 from django.db import models
+
+
 # Create your models here.
 
 
 class RadiusGroup(models.Model):
     id = models.UUIDField(primary_key=True)
-    groupName = models.CharField(max_length=255, unique=True)
+    group_name = models.CharField(max_length=255, unique=True)
     priority = models.IntegerField(default=1)
-    creationDate = models.DateField(null=True)
-    modificationDate = models.DateField(null=True)
+    creation_date = models.DateField(null=True)
+    modification_date = models.DateField(null=True)
     notes = models.CharField(max_length=64, blank=True)
+
+    def __str__(self):
+        return self.group_name
+
+    class Meta:
+        db_table = 'radiusgroup'
 
 
 class RadiusGroupUsers(models.Model):
-    userName = models.CharField(max_length=64, unique=True)
+    user_name = models.CharField(max_length=64, unique=True)
     id = models.UUIDField(primary_key=True)
-    groupName = models.CharField(max_length=255, unique=True)
-    radiusReplies = models.ManyToManyField('RadiusReplies', blank=True)
-    radiusChecks = models.ManyToManyField('RadiusChecks', blank=True)
+    group_name = models.CharField(max_length=255, unique=True)
+    radius_replies = models.ManyToManyField('RadiusReplies', blank=True)
+    radius_checks = models.ManyToManyField('RadiusChecks', blank=True)
+
+    class Meta:
+        db_table = 'radiusgroupusers'
+
+    def __str__(self):
+        return self.user_name
 
 
 class RadiusReplies(models.Model):
-    userName = models.CharField(max_length=64)
+    user_name = models.CharField(max_length=64)
     value = models.CharField(max_length=40)
     op = models.CharField(max_length=2)
     attribute = models.CharField(max_length=64)
+
+    class Meta:
+        db_table = 'radiusreplies'
+
+    def __str__(self):
+        return self.user_name
 
 
 class RadiusChecks(models.Model):
-    userName = models.CharField(max_length=64)
+    user_name = models.CharField(max_length=64)
     value = models.CharField(max_length=40)
     op = models.CharField(max_length=2)
     attribute = models.CharField(max_length=64)
 
+    class Meta:
+        db_table = 'radiuschecks'
+
+    def __str__(self):
+        return self.user_name
+
 
 class RadiusAccounting(models.Model):
-    radAcctId = models.BigIntegerField(primary_key=True)
-    acctSessionId = models.CharField(max_length=32)
-    acctUniqueId = models.CharField(max_length=32)
-    userName = models.CharField(max_length=64)
+    rad_acct_id = models.BigIntegerField(primary_key=True)
+    acct_session_id = models.CharField(max_length=32)
+    acct_unique_id = models.CharField(max_length=32)
+    user_name = models.CharField(max_length=64)
     realm = models.CharField(max_length=64, null=True)
-    nasIpAddress = models.CharField(max_length=15)
-    nasPortId = models.CharField(max_length=15, null=True)
-    nasPortType = models.CharField(max_length=32)
-    acctStartTime = models.DateTimeField()
-    acctStopTime = models.DateTimeField(null=True)
-    acctSessionTime = models.IntegerField(null=True)
-    acctAuthentic = models.CharField(max_length=32, null=True)
-    connectionInfoStart = models.CharField(max_length=50, null=True)
-    connectionInfoStop = models.CharField(max_length=50, null=True)
-    acctInputOctets = models.BigIntegerField(null=True)
-    acctOutputOctets = models.BigIntegerField(null=True)
+    nas_ip_address = models.CharField(max_length=15)
+    nas_port_id = models.CharField(max_length=15, null=True)
+    nas_port_type = models.CharField(max_length=32)
+    acct_start_time = models.DateTimeField()
+    acct_stop_time = models.DateTimeField(null=True)
+    acct_session_time = models.IntegerField(null=True)
+    acct_authentic = models.CharField(max_length=32, null=True)
+    connection_info_start = models.CharField(max_length=50, null=True)
+    connection_info_stop = models.CharField(max_length=50, null=True)
+    acct_input_octets = models.BigIntegerField(null=True)
+    acct_output_octets = models.BigIntegerField(null=True)
     callingStationId = models.CharField(max_length=50)
     calledStationId = models.CharField(max_length=50)
-    acctTerminateCause = models.CharField(max_length=32)
-    serviceType = models.CharField(max_length=32, null=True)
-    framedProtocol = models.CharField(max_length=32, null=True)
-    framedIpAddress = models.CharField(max_length=15)
-    acctStartDelay = models.IntegerField(null=True)
-    acctStopDelay = models.IntegerField(null=True)
-    xAscendSessionSvrKey = models.CharField(max_length=10, null=True)
+    acct_terminate_cause = models.CharField(max_length=32)
+    service_type = models.CharField(max_length=32, null=True)
+    framed_protocol = models.CharField(max_length=32, null=True)
+    framed_ip_address = models.CharField(max_length=15)
+    acct_start_delay = models.IntegerField(null=True)
+    acct_stop_delay = models.IntegerField(null=True)
+    xascend_session_svrkey = models.CharField(max_length=10, null=True)
+
+    class Meta:
+        db_table = 'radiusaccounting'
+
+    def __str__(self):
+        return self.acct_unique_id
 
 
 class Nas(models.Model):
-    nasName = models.CharField(max_length=128, unique=True, help_text='NAS Name (or IP address)')
-    shortName = models.CharField(max_length=32)
+    nas_name = models.CharField(max_length=128, unique=True, help_text='NAS Name (or IP address)')
+    short_name = models.CharField(max_length=32)
     type = models.CharField(max_length=30)
     secret = models.CharField(max_length=60, help_text='Shared Secret')
     ports = models.IntegerField(blank=True, null=True)
     community = models.CharField(max_length=50, blank=True, null=True)
+
+    class Meta:
+        db_table = 'nas'
+
+    def __str__(self):
+        return self.nas_name
