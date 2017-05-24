7c57b7172efb0657e357a0ef099e8561045db918
Fiorella De Luca <deluca_fiorella@yahoo.it>
2017-05-24 14:54:12 +0200

    [admin] Added admin classes

--- django_freeradius/admin.py
+++ django_freeradius/admin.py
@@ -1,3 +1,34 @@
+from django.contrib import admin
+
+from .models import (Nas, RadiusAccounting, RadiusChecks, RadiusGroup,
+                     RadiusGroupUsers, RadiusReplies)
 
 
-# Register your models here.
+@admin.register(RadiusGroup)
+class RadiusGroupAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(RadiusGroupUsers)
+class RadiusGroupUsers(admin.ModelAdmin):
+    pass
+
+
+@admin.register(RadiusChecks)
+class RadiusChecks(admin.ModelAdmin):
+    pass
+
+
+@admin.register(RadiusReplies)
+class RadiusReplies(admin.ModelAdmin):
+    pass
+
+
+@admin.register(RadiusAccounting)
+class RadiusAccounting(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Nas)
+class Nas(admin.ModelAdmin):
+    pass
