1e40453d5620c4501a979e3b02b524d0de1acd3d
Fiorella De Luca <deluca_fiorella@yahoo.it>
2017-06-24 20:37:21 +0200

    [runtests] added sample_radius to runtests

--- runtests.py
+++ runtests.py
@@ -12,4 +12,5 @@ if __name__ == "__main__":
     args = sys.argv
     args.insert(1, "test")
     args.insert(2, "django_freeradius")
+    args.insert(3, "sample_radius")
     execute_from_command_line(args)
