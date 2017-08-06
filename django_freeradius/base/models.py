bc6c2125467ea6145b2a7b8e5fb08beeec1ada64
SLY <pavelsly@gmail.com>
2017-08-06 20:06:12 +0300

    accounting start/stop delay fields

--- django_freeradius/base/models.py
+++ django_freeradius/base/models.py
@@ -250,6 +250,12 @@ class AbstractRadiusAccounting(models.Model):
                                                      # but that wouldn't work on PostgreSQL
                                                      null=True,
                                                      blank=True)
+    acct_start_delay = models.IntegerField(verbose_name=_('accounting start delay'),
+                                           null=True,
+                                           db_column='acctstartdelay')
+    acct_stop_delay = models.IntegerField(verbose_name=_('accounting stop delay'),
+                                          null=True,
+                                          db_column='acctstopdelay')
 
     class Meta:
         db_table = 'radacct'
