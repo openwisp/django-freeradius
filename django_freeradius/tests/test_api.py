684d1cbfbd0e3522bbfd0dea2162040146dad043
Federico Capoano <nemesis@ninux.org>
2017-08-09 16:53:04 +0200

    [tests] Minor renaming for consistency

--- django_freeradius/tests/test_api.py
+++ django_freeradius/tests/test_api.py
@@ -7,14 +7,14 @@ RadiusPostAuth = swapper.load_model("django_freeradius", "RadiusPostAuth")
 
 
 class TestApi(TestCase):
-    def test_authorize(self):
+    def test_authorize_200(self):
         User.objects.create_user(username='molly', password='barbar')
         response = self.client.post(reverse('freeradius:authorize'),
                                     {'username': 'molly', 'password': 'barbar'})
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.data, {'control:Auth-Type': 'Accept'})
 
-    def test_unauthorize(self):
+    def test_authorize_401(self):
         response = self.client.post(reverse('freeradius:authorize'),
                                     {'username': 'baldo', 'password': 'ugo'})
         self.assertEqual(response.status_code, 401)
