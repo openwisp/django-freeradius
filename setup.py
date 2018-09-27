47f4285163a0bd1f040337278c995abdbdeec590
Arushit Mudgal <arushitmudgal@gmail.com>
2018-09-27 15:29:04 +0530

    [setup] Updating python version in setup.py #187
    The python version mentioned in setup.py were 2.7 and 3.4 which had to be
    changed to 3.5 and 3.6 because Django 2.1 supports them.
    Also updating python version in travis.
    
    Closes #187

--- setup.py
+++ setup.py
@@ -59,7 +59,7 @@ setup(
         'Operating System :: OS Independent',
         'Framework :: Django',
         'Topic :: System :: Networking',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
     ]
 )
